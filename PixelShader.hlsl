163963bc87370dfc056724bdbd98cfb52dea740f
Sgedelta <Sgeaston04@gmail.com>
2025-03-04 20:16:28 -0500

    small pixel shader change

--- PixelShader.hlsl
+++ PixelShader.hlsl
@@ -38,5 +38,5 @@ float4 main(VertexToPixel input) : SV_TARGET
 	// - This color (like most values passing through the rasterizer) is 
 	//   interpolated for each pixel between the corresponding vertices 
 	//   of the triangle we're rendering
-    return float4(input.uv.x, input.uv.y, 0, 0);
+    return float4(input.uv.x, input.uv.y, 1, 1);
 }
